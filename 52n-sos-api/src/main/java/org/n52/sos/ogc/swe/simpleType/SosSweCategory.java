509bbe90b4ce1ccffd7f4dbf04edbdfc886ae961
EikeJuerrens <EikeJuerrens@9b2695e2-e135-4693-8c03-170fd80ef316>
2012-11-20 16:25:44 +0000

    add new swe simple types for Category and TimeRange
    git-svn-id: https://svn.52north.org/svn/swe/main/SOS/Service/branches/52n-sos-400-refactored@14001 9b2695e2-e135-4693-8c03-170fd80ef316

---
+++ 52n-sos-api/src/main/java/org/n52/sos/ogc/swe/simpleType/SosSweCategory.java
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2012
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+package org.n52.sos.ogc.swe.simpleType;
+
+import org.n52.sos.ogc.swe.SWEConstants.SweSimpleType;
+
+/**
+ * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
+ *
+ */
+public class SosSweCategory extends SosSweAbstractSimpleType {
+
+    @Override
+    public SweSimpleType getSimpleType()
+    {
+        // TODO Auto-generated method "getSimpleType"
+        return null;
+    }
+
+    @Override
+    public SosSweQuality getQuality()
+    {
+        // TODO Auto-generated method "getQuality"
+        return null;
+    }
+
+    @Override
+    public void setQuality(SosSweQuality quality)
+    {
+        // TODO Auto-generated method "setQuality"
+
+    }
+
+    @Override
+    public String getValue()
+    {
+        // TODO Auto-generated method "getValue"
+        return null;
+    }
+
+    @Override
+    public void setValue(String value)
+    {
+        // TODO Auto-generated method "setValue"
+
+    }
+    
+    public void setCodeSpace(String codeSpace)
+    {
+        // TODO Auto-generated method "setCodeSpace"
+    }
+    
+    public String getCodeSpace()
+    {
+        // TODO Auto-generated method "getCodeSpace"
+        return null;
+    }
+
+}
