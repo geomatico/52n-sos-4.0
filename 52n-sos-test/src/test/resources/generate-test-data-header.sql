97da9582bf71c4e05265ccf74091a21430005c60
ChristianAutermann <ChristianAutermann@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-01-15 13:05:27 +0000

    Remove nil component element from sensor description
    git-svn-id: https://svn.52north.org/svn/swe/main/SOS/Service/branches/52n-sos-400-refactored@14789 9b2695e2-e135-4693-8c03-170fd80ef316

--- 52n-sos-test/src/test/resources/generate-test-data-header.sql
+++ 52n-sos-test/src/test/resources/generate-test-data-header.sql
@@ -24,21 +24,21 @@
 
 CREATE OR REPLACE FUNCTION get_observation_type(text) RETURNS bigint AS
 $$
-    SELECT observation_type_id FROM observation_type 
+    SELECT observation_type_id FROM observation_type
     WHERE observation_type = 'http://www.opengis.net/def/observationType/OGC-OM/2.0/OM_'::text || $1;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_procedure(text) RETURNS bigint AS
 $$
-    SELECT procedure_id FROM procedure WHERE identifier = $1; 
+    SELECT procedure_id FROM procedure WHERE identifier = $1;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_feature_of_interest_type(text) RETURNS bigint AS
 $$
-    SELECT feature_of_interest_type_id 
-    FROM feature_of_interest_type 
+    SELECT feature_of_interest_type_id
+    FROM feature_of_interest_type
     WHERE feature_of_interest_type = $1;
 $$
 LANGUAGE 'sql';
@@ -57,7 +57,7 @@ LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_sensor_ml_description_format() RETURNS bigint AS
 $$
-    SELECT procedure_description_format_id FROM procedure_description_format 
+    SELECT procedure_description_format_id FROM procedure_description_format
     WHERE procedure_description_format = 'http://www.opengis.net/sensorML/1.0.1'::text;
 $$
 LANGUAGE 'sql';
@@ -69,7 +69,7 @@ $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_observable_property(text) RETURNS bigint AS
-$$ 
+$$
     SELECT observable_property_id FROM observable_property WHERE identifier = $1;
 $$
 LANGUAGE 'sql';
@@ -147,12 +147,12 @@ LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_feature_of_interest(text, numeric, numeric) RETURNS bigint AS
 $$
-    INSERT INTO feature_of_interest(feature_of_interest_type_id, identifier, name, geom, description_xml) 
-    SELECT get_spatial_sampling_feature_type('Point'), $1, $1, ST_GeomFromText('POINT(' || $2 || $3 || ')', 4326), 
-'<sams:SF_SpatialSamplingFeature 
+    INSERT INTO feature_of_interest(feature_of_interest_type_id, identifier, name, geom, description_xml)
+    SELECT get_spatial_sampling_feature_type('Point'), $1, $1, ST_GeomFromText('POINT(' || $2 || $3 || ')', 4326),
+'<sams:SF_SpatialSamplingFeature
     xmlns:xlink="http://www.w3.org/1999/xlink"
-    xmlns:sams="http://www.opengis.net/samplingSpatial/2.0" 
-    xmlns:sf="http://www.opengis.net/sampling/2.0" 
+    xmlns:sams="http://www.opengis.net/samplingSpatial/2.0"
+    xmlns:sf="http://www.opengis.net/sampling/2.0"
     xmlns:gml="http://www.opengis.net/gml/3.2" gml:id="ssf_'::text || $1 || '">
     <gml:identifier codeSpace="">'::text || $1 || '</gml:identifier>
     <sf:type xlink:href="http://www.opengis.net/def/samplingFeatureType/OGC-OM/2.0/SF_SamplingPoint"/>
@@ -178,7 +178,7 @@ LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION create_sensor_description(text, text, numeric, numeric, numeric) RETURNS text AS
 $$
-    SELECT 
+    SELECT
 '<sml:SensorML version="1.0.1"
   xmlns:sml="http://www.opengis.net/sensorML/1.0.1"
   xmlns:gml="http://www.opengis.net/gml"
@@ -237,7 +237,6 @@ $$
           </sml:output>
         </sml:OutputList>
       </sml:outputs>
-      <sml:components xsi:nil="true"/>
     </sml:System>
   </sml:member>
 </sml:SensorML>'::text;
@@ -246,20 +245,20 @@ LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_procedure(text,timestamp,text,numeric,numeric,numeric,bigint,bigint) RETURNS bigint AS
 $$
-    INSERT INTO procedure(identifier, procedure_description_format_id, deleted) SELECT 
+    INSERT INTO procedure(identifier, procedure_description_format_id, deleted) SELECT
         $1, get_sensor_ml_description_format(), false WHERE $1 NOT IN (
             SELECT identifier FROM procedure WHERE identifier = $1);
-    INSERT INTO valid_procedure_time(procedure_id, start_time, description_xml) 
+    INSERT INTO valid_procedure_time(procedure_id, start_time, description_xml)
         SELECT get_procedure($1), $2, create_sensor_description($1, $3, $4, $5, $6)
         WHERE get_procedure($1) NOT IN (
             SELECT procedure_id FROM valid_procedure_time WHERE procedure_id = get_procedure($1));
-    INSERT INTO procedure_has_observation_type(procedure_id, observation_type_id) 
-        SELECT get_procedure($1), $7 
-        WHERE $7 NOT IN (SELECT observation_type_id FROM procedure_has_observation_type 
+    INSERT INTO procedure_has_observation_type(procedure_id, observation_type_id)
+        SELECT get_procedure($1), $7
+        WHERE $7 NOT IN (SELECT observation_type_id FROM procedure_has_observation_type
                  WHERE procedure_id = get_procedure($1) AND observation_type_id = $7);
-    INSERT INTO procedure_has_feature_of_interest_type(procedure_id, feature_of_interest_type_id) 
+    INSERT INTO procedure_has_feature_of_interest_type(procedure_id, feature_of_interest_type_id)
         SELECT get_procedure($1), $8
-        WHERE $8 NOT IN (SELECT feature_of_interest_type_id FROM procedure_has_feature_of_interest_type 
+        WHERE $8 NOT IN (SELECT feature_of_interest_type_id FROM procedure_has_feature_of_interest_type
                  WHERE procedure_id = get_procedure($1) AND feature_of_interest_type_id = $8);
     SELECT get_procedure($1);
 $$
@@ -277,31 +276,31 @@ $$
     SELECT $3, $1 WHERE $3 NOT IN (
         SELECT offering_id FROM offering_has_allowed_observation_type WHERE offering_id = $3);
     INSERT INTO observation_constellation(observation_type_id, procedure_id, offering_id, observable_property_id)
-    SELECT $1,$2,$3,$4 WHERE $1 NOT IN (SELECT observation_type_id 
-        FROM observation_constellation  WHERE observation_type_id = $1 
+    SELECT $1,$2,$3,$4 WHERE $1 NOT IN (SELECT observation_type_id
+        FROM observation_constellation  WHERE observation_type_id = $1
           AND procedure_id = $2 AND offering_id = $3 AND observable_property_id = $4);
-    SELECT observation_constellation_id FROM observation_constellation  
+    SELECT observation_constellation_id FROM observation_constellation
         WHERE observation_type_id = $1  AND procedure_id = $2 AND offering_id = $3 AND observable_property_id = $4;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_observation_constellation(text,text,text,text) RETURNS bigint AS
 $$
-    SELECT insert_observation_constellation(get_observation_type($1), 
+    SELECT insert_observation_constellation(get_observation_type($1),
         get_procedure($2), get_offering($3), get_observable_property($4));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_observation_constellation(bigint,bigint,bigint,bigint) RETURNS bigint AS
 $$
-    SELECT observation_constellation_id FROM observation_constellation  
+    SELECT observation_constellation_id FROM observation_constellation
         WHERE observation_type_id = $1  AND procedure_id = $2 AND offering_id = $3 AND observable_property_id = $4;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION get_observation_constellation(text,text,text,text) RETURNS bigint AS
 $$
-    SELECT get_observation_constellation(get_observation_type($1), 
+    SELECT get_observation_constellation(get_observation_type($1),
         get_procedure($2), get_offering($3), get_observable_property($4));
 $$
 LANGUAGE 'sql';
@@ -320,76 +319,76 @@ LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_numeric_observation(bigint, numeric) RETURNS VOID AS
 $$
-    INSERT INTO observation_has_numeric_value(observation_id, numeric_value_id) 
+    INSERT INTO observation_has_numeric_value(observation_id, numeric_value_id)
         SELECT $1, insert_numeric_value($2) WHERE $1 NOT IN (
-            SELECT observation_id FROM observation_has_numeric_value 
+            SELECT observation_id FROM observation_has_numeric_value
             WHERE observation_id = $1 AND numeric_value_id = insert_numeric_value($2));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_observation(bigint, text, text, timestamp) RETURNS bigint AS
-$$ 
+$$
     INSERT INTO observation(observation_constellation_id, feature_of_interest_id, unit_id, phenomenon_time_start, phenomenon_time_end, result_time)
     SELECT $1, get_feature_of_interest($2), get_unit($3), $4, $4, $4 WHERE $1 NOT IN (
         SELECT observation_constellation_id FROM observation
-        WHERE observation_constellation_id = $1 AND feature_of_interest_id = get_feature_of_interest($2) 
+        WHERE observation_constellation_id = $1 AND feature_of_interest_id = get_feature_of_interest($2)
                 AND unit_id = get_unit($3) AND phenomenon_time_start = $4 AND phenomenon_time_end = $4 AND result_time = $4);
 
-    SELECT observation_id FROM observation 
+    SELECT observation_id FROM observation
     WHERE feature_of_interest_id = get_feature_of_interest($2)
-        AND observation_constellation_id = $1 AND unit_id = get_unit($3) 
+        AND observation_constellation_id = $1 AND unit_id = get_unit($3)
         AND phenomenon_time_start = $4;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_boolean_observation(bigint, boolean) RETURNS VOID AS
-$$ 
+$$
     INSERT INTO observation_has_boolean_value(observation_id, boolean_value_id) SELECT $1, get_boolean_value($2)
-    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_boolean_value 
+    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_boolean_value
             WHERE observation_id = $1 AND boolean_value_id = get_boolean_value($2));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_count_observation(bigint, int) RETURNS VOID AS
-$$ 
+$$
     INSERT INTO observation_has_count_value(observation_id, count_value_id) SELECT $1, insert_count_value($2)
-    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_count_value 
+    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_count_value
             WHERE observation_id = $1 AND count_value_id = insert_count_value($2));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_text_observation(bigint, text) RETURNS VOID AS
-$$ 
+$$
     INSERT INTO observation_has_text_value(observation_id, text_value_id) SELECT $1, insert_text_value($2)
-    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_text_value 
+    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_text_value
             WHERE observation_id = $1 AND text_value_id = insert_text_value($2));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_category_observation(bigint, text) RETURNS VOID AS
-$$ 
+$$
     INSERT INTO observation_has_category_value(observation_id, category_value_id) SELECT $1, insert_category_value($2)
-    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_category_value 
+    WHERE $1 NOT IN (SELECT observation_id FROM observation_has_category_value
         WHERE observation_id = $1 AND category_value_id = insert_category_value($2));
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_result_template(bigint,bigint,text,text,text) RETURNS bigint AS
-$$ 
+$$
     INSERT INTO result_template(observation_constellation_id, feature_of_interest_id, identifier, result_structure, result_encoding)
     SELECT  $1, $2, $3, $4, $5 WHERE $3 NOT IN (
-        SELECT identifier FROM result_template 
-        WHERE observation_constellation_id = $1 
-            AND feature_of_interest_id = $2 
-            AND identifier = $3 
-            AND result_structure = $4 
+        SELECT identifier FROM result_template
+        WHERE observation_constellation_id = $1
+            AND feature_of_interest_id = $2
+            AND identifier = $3
+            AND result_structure = $4
             AND result_encoding = $5);
     SELECT result_template_id FROM result_template WHERE identifier = $3;
 $$
 LANGUAGE 'sql';
 
 CREATE OR REPLACE FUNCTION insert_result_template(text,text,text,text,text,text) RETURNS bigint AS
-$$ 
+$$
     SELECT insert_result_template(get_observation_constellation($1, $2, $3, $4), get_feature_of_interest($5),
         $2 || '/template/1'::text,
         '<swe:DataRecord xmlns:swe="http://www.opengis.net/swe/2.0" xmlns:xlink="http://www.w3.org/1999/xlink">
