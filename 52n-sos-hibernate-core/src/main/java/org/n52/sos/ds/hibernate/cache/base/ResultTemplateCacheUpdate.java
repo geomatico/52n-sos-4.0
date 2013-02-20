b6b08d752b901a6720f66d7a0d3c5c95e15b9251
EikeJuerrens <EikeJuerrens@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-02-20 13:18:00 +0000

    - updated javadoc - added retrieval of "offering --> result template" relation from database
    git-svn-id: https://svn.52north.org/svn/swe/main/SOS/Service/branches/52n-sos-400-refactored@15605 9b2695e2-e135-4693-8c03-170fd80ef316

--- 52n-sos-hibernate-core/src/main/java/org/n52/sos/ds/hibernate/cache/base/ResultTemplateCacheUpdate.java
+++ 52n-sos-hibernate-core/src/main/java/org/n52/sos/ds/hibernate/cache/base/ResultTemplateCacheUpdate.java
@@ -25,25 +25,52 @@ package org.n52.sos.ds.hibernate.cache.base;
 
 import static org.n52.sos.ds.hibernate.util.HibernateCriteriaQueryUtilities.getResultTemplateObjects;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.n52.sos.ds.hibernate.cache.CacheUpdate;
 import org.n52.sos.ds.hibernate.entities.ResultTemplate;
+import org.n52.sos.util.Action;
+import org.n52.sos.util.CollectionHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
- *
+ * When executing this &auml;ction (see {@link Action}), the following relations are added, settings are updated in cache:<ul>
+ * <li>Result template identifier</li>
+ * <li>Procedure &rarr; 'Result template identifier' relation</li>
+ * </ul>
  * @author Christian Autermann <c.autermann@52north.org>
+ * @author <a href="mailto:e.h.juerrens@52north.org">Eike Hinderk J&uuml;rrens</a>
+ * @since 4.0.0
  */
 public class ResultTemplateCacheUpdate extends CacheUpdate {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(ResultTemplateCacheUpdate.class);
 
     @Override
-    public void execute() {
+    public void execute()
+    {
         List<ResultTemplate> resultTemplateObjects = getResultTemplateObjects(getSession());
-        List<String> resultTemplates = new ArrayList<String>(resultTemplateObjects.size());
-        for (ResultTemplate resultTemplateObject : resultTemplateObjects) {
-            resultTemplates.add(resultTemplateObject.getIdentifier());
+        for (ResultTemplate resultTemplateObject : resultTemplateObjects)
+        {
+            String resultTemplateIdentifier = resultTemplateObject.getIdentifier();
+            getCache().getResultTemplates().add(resultTemplateIdentifier);
+            LOGGER.debug("ResultTemplate identifier '{}' added to cache? {}",
+            		resultTemplateIdentifier,
+            		getCache().getResultTemplates().contains(resultTemplateIdentifier));
+            
+            String offeringIdentifier = resultTemplateObject.getObservationConstellationOfferingObservationType().getOffering().getIdentifier();
+            if (!getCache().getKOfferingVResultTemplates().containsKey(offeringIdentifier))
+            {
+            	Collection<String> resultTemplateIdentifiers = CollectionHelper.synchronizedArrayList(1);
+            	getCache().getKOfferingVResultTemplates().put(offeringIdentifier, resultTemplateIdentifiers);
+            }
+            getCache().getKOfferingVResultTemplates().get(offeringIdentifier).add(resultTemplateIdentifier);
+            LOGGER.debug("Offering '{}' to result template '{}' relation added to cache? {}",
+            		offeringIdentifier,
+            		resultTemplateIdentifier,
+            		getCache().getKOfferingVResultTemplates().get(offeringIdentifier).contains(resultTemplateIdentifier));
         }
-        getCache().setResultTemplates(resultTemplates);
     }
 }
