0b24cbcb8e9ea95adace5730cd7563c311fe052d
ChristianAutermann <ChristianAutermann@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-04-07 13:42:50 +0000

    fix license headers
    git-svn-id: https://svn.52north.org/svn/swe/main/SOS/Service/branches/52n-sos-400-refactored@20090 9b2695e2-e135-4693-8c03-170fd80ef316

--- 52n-sos-api/src/main/java/org/n52/sos/service/profile/ProfileHandler.java
+++ 52n-sos-api/src/main/java/org/n52/sos/service/profile/ProfileHandler.java
@@ -18,7 +18,8 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * this program (see gnu-gpl v2.txt). If not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 0T2111-1307, USA or * visit the Free Software Foundation web page, http://www.fsf.org.
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
  */
 package org.n52.sos.service.profile;
 
