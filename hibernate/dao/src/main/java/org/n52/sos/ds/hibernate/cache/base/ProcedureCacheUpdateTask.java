3645c3ebf850ba17b72fcc5ec11600bb799a4132
ShaneStClair <ShaneStClair@9b2695e2-e135-4693-8c03-170fd80ef316>
2013-05-20 23:38:47 +0000

    Make abstract queuing cache update class (commit forgotten classes)
    git-svn-id: https://svn.52north.org/svn/swe/main/SOS/Service/branches/52n-sos-400-refactored@20666 9b2695e2-e135-4693-8c03-170fd80ef316

---
+++ hibernate/dao/src/main/java/org/n52/sos/ds/hibernate/cache/base/ProcedureCacheUpdateTask.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (C) 2013
+ * by 52 North Initiative for Geospatial Open Source Software GmbH
+ *
+ * Contact: Andreas Wytzisk
+ * 52 North Initiative for Geospatial Open Source Software GmbH
+ * Martin-Luther-King-Weg 24
+ * 48155 Muenster, Germany
+ * info@52north.org
+ *
+ * This program is free software; you can redistribute and/or modify it under
+ * the terms of the GNU General Public License version 2 as published by the
+ * Free Software Foundation.
+ *
+ * This program is distributed WITHOUT ANY WARRANTY; even without the implied
+ * WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (see gnu-gpl v2.txt). If not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA or
+ * visit the Free Software Foundation web page, http://www.fsf.org.
+ */
+package org.n52.sos.ds.hibernate.cache.base;
+
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+
+import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
+import org.n52.sos.cache.WritableContentCache;
+import org.n52.sos.ds.hibernate.ThreadLocalSessionFactory;
+import org.n52.sos.ds.hibernate.entities.Observation;
+import org.n52.sos.ds.hibernate.entities.ObservationConstellation;
+import org.n52.sos.ds.hibernate.entities.Procedure;
+import org.n52.sos.ds.hibernate.util.HibernateCriteriaQueryUtilities;
+import org.n52.sos.exception.ows.concrete.GenericThrowableWrapperException;
+import org.n52.sos.ogc.ows.OwsExceptionReport;
+import org.n52.sos.util.CollectionHelper;
+import org.n52.sos.util.RunnableAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class ProcedureCacheUpdateTask extends RunnableAction {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProcedureCacheUpdateTask.class);
+    private CountDownLatch countDownLatch;
+    private ThreadLocalSessionFactory sessionFactory;
+    private List<OwsExceptionReport> errors;
+    private Procedure procedure;
+    private WritableContentCache cache;
+
+    ProcedureCacheUpdateTask(
+            CountDownLatch countDownLatch, 
+            ThreadLocalSessionFactory sessionFactory,
+            WritableContentCache offeringCache,
+            Procedure procedure,
+            List<OwsExceptionReport> error) {
+        this.countDownLatch = countDownLatch;
+        this.sessionFactory = sessionFactory;
+        this.cache = offeringCache;
+        this.procedure = procedure;
+        this.errors = error;
+    }
+
+    protected void getProcedureInformationFromDbAndAddItToCacheMaps(Session session) throws OwsExceptionReport {
+        final String id = procedure.getIdentifier();
+        final Set<ObservationConstellation> ocs = getObservationConstellations(session, procedure);
+        cache.addProcedure(id);
+        cache.setOfferingsForProcedure(id, getOfferingIdentifiers(ocs));
+        cache.setObservablePropertiesForProcedure(id, getObservableProperties(ocs));
+        cache.addParentProcedures(id, getProcedureIdentifiers(procedure.getParentProcedures()));
+        cache.setObservationIdentifiersForProcedure(id, getObservationIdentifiers(session, id));        
+
+        // Temporal Envelope
+        cache.setMinPhenomenonTimeForProcedure(id, HibernateCriteriaQueryUtilities
+                .getMinDate4Procedure(id, session));
+        cache.setMaxPhenomenonTimeForProcedure(id, HibernateCriteriaQueryUtilities
+                .getMaxDate4Procedure(id, session));
+    }
+
+    @Override
+    public void execute() {
+        try {
+            getProcedureInformationFromDbAndAddItToCacheMaps(sessionFactory.getSession());
+        } catch (OwsExceptionReport owse) {
+            errors.add(owse);
+        } catch (Exception e) {
+            errors.add(new GenericThrowableWrapperException(e)
+                    .withMessage("Error while processing procedure cache update task!"));
+        } finally {
+            LOGGER.debug("ProcedureCacheUpdateTask finished, latch.countDown().");
+            countDownLatch.countDown();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Set<ObservationConstellation> getObservationConstellations(Session session, Procedure procedure) {
+        return CollectionHelper.asSet(session.createCriteria(ObservationConstellation.class)
+                .add(Restrictions.eq(ObservationConstellation.PROCEDURE, procedure)).list());
+    }
+    
+    protected Set<String> getObservableProperties(Set<ObservationConstellation> set) {
+        Set<String> observableProperties = new HashSet<String>(set.size());
+        for (ObservationConstellation observationConstellation : set) {
+            observableProperties.add(observationConstellation.getObservableProperty().getIdentifier());
+        }
+        return observableProperties;
+    }
+
+    protected Set<String> getProcedureIdentifiers(Set<Procedure> procedures) {
+        Set<String> identifiers = new HashSet<String>(procedures.size());
+        for (Procedure procedure : procedures) {
+            identifiers.add(procedure.getIdentifier());
+        }
+        return identifiers;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected Set<String> getObservationIdentifiers(Session session, String procedureIdentifier) {
+        return CollectionHelper.asSet(session.createCriteria(Observation.class)
+                .setProjection(Projections.distinct(Projections.property(Observation.IDENTIFIER)))
+                .add(Restrictions.eq(Observation.DELETED, false))
+                .createCriteria(Observation.PROCEDURE)
+                .add(Restrictions.eq(Procedure.IDENTIFIER, procedureIdentifier))
+                .list());
+    }
+    
+    protected Set<String> getOfferingIdentifiers(Set<ObservationConstellation> observationConstellations) {
+        Set<String> offerings = new HashSet<String>(observationConstellations.size());
+        for (ObservationConstellation oc : observationConstellations) {
+            offerings.add(oc.getOffering().getIdentifier());
+        }
+        return offerings;
+    }
+    
+}
